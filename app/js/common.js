0646b93ca4d6c6a6bc6fba82abad8ce374717a0d
Viktoria <zaharova181114@gmail.com>
2022-10-04 11:09:23 +0300

    add page

--- app/js/common.js
+++ app/js/common.js
@@ -6,3 +6,11 @@ $(document).ready(function () {
 });
 
 $('[name="phone"]').mask('+7(999) 999-99-99');
+
+$('.btn-burger').on('click', function () {
+   $('.mobile-menu').fadeToggle();
+});
+
+$('.mobile-menu__close').on('click', function () {
+    $('.mobile-menu').fadeToggle();
+});
