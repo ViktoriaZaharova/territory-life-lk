0e6b4b21c4d6abddb25ec346e3c3e168083c5926
Viktoria <zaharova181114@gmail.com>
2022-10-31 09:22:39 +0200

    add slider

--- app/js/common.js
+++ app/js/common.js
@@ -8,9 +8,18 @@ $(document).ready(function () {
 $('[name="phone"]').mask('+7(999) 999-99-99');
 
 $('.btn-burger').on('click', function () {
-   $('.mobile-menu').fadeToggle();
+    $('.mobile-menu').fadeToggle();
 });
 
 $('.mobile-menu__close').on('click', function () {
     $('.mobile-menu').fadeToggle();
+});
+
+$('.wrapper-image').slick({
+    slidesToShow: 1,
+    fade: true,
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
 });
